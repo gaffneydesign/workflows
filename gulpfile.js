0871c4f7f01a90ca44008c4b5bc981e176d1805d
John Gaffney <john@jgaffney.com>
2016-10-14 16:58:14 -0700

    Minified HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 var   env,
       coffeeSources,
@@ -69,8 +70,8 @@ gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
-	gulp.watch(htmlSources, ['html']);
-	gulp.watch(htmlSources, ['json']);
+	gulp.watch('builds/development/*.html', ['html']);
+	gulp.watch(jsonSources, ['json']);
 })
 
 gulp.task('connect', function() {
@@ -81,7 +82,9 @@ gulp.task('connect', function() {
 })
 
 gulp.task('html', function() {
-  gulp.src(htmlSources)
+  gulp.src('builds/development/*.html')
+    .pipe(gulpif(env=== 'production', minifyHTML()))
+    .pipe(gulpif(env=== 'production', gulp.dest(outputDir)))
     .pipe(connect.reload())
 })
 
